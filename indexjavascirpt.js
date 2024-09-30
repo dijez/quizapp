54ff912b92c7536bfcf9dd8dd3c8900bc9bed1d5
dijez <khadijaahmad2k21@gmail.com>
2024-09-30 11:01:25 +0100

    Update indexjavascirpt.js
    function to start quiz

--- indexjavascirpt.js
+++ indexjavascirpt.js
@@ -28,5 +28,21 @@ nextButton.addEventListener('click', () => {
   setNextQuestion()
 })
 
+// Function to start the quiz
+function startGame() {
+    score = 0 // Reset score at the start of each game
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+  }
   
+  // Function to set the next question
+  function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+  }
+  
+    
   
